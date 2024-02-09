7cff7d93d006527c3c5c94c0c68124244c270719
ramillers <rmillesantana@gmail.com>
2024-02-09 15:24:23 -0300

    feat: add absentPercentage calculation and grade calculation for Final Approval (naf)

--- index.js
+++ index.js
@@ -36,31 +36,52 @@ async function getSpreadsheetData() {
 
     const { data } = await googleSheets.spreadsheets.values.get({
         spreadsheetId,
-        range: "A4:F",
+        range: "A2:F",
     });
 
-    const rows = data.values; 
+    const values = data.values; 
+    console.log(values);
+    const rows = values.slice(2); 
+    const classHours = parseInt(values[0][0].split(":")[1]); 
+
+    //console.log(classHours);
 
     if (rows.length) {
+
         const newData = rows.map(row => {
+            const studentAbsence = parseFloat(row[2]);
             const p1 = parseFloat(row[3]);
             const p2 = parseFloat(row[4]);
             const p3 = parseFloat(row[5]); 
-
-            const media = (p1 + p2 + p3) / 3; 
-
-            let situacao;
-            if ( media < 50) {
-                situacao = "Reprovado por Nota!";
-            } else  if ( media >=50 && media<70) {
-                situacao = "Exame final!";
+        
+            console.log("Dados do aluno:", { studentAbsence, p1, p2, p3 });
+        
+            const media = (p1 + p2 + p3) / 3;
+            const absentPercentage = (studentAbsence / classHours) * 100;
+        
+            console.log("Média:", media);
+            console.log("Percentual de Faltas:", absentPercentage);
+        
+            let naf = 0; // Inicialize o naf como 0
+        
+            let situation;
+            if (absentPercentage > 25) {
+                situation = "Reprovado por Falta!";
+            } else if (media < 50) {
+                situation = "Reprovado por Nota!";
+            } else if (media >= 50 && media < 70) {
+                situation = "Exame final!";
+                // Calcular naf apenas para a situação de "Exame final!"
+                naf = Math.ceil(100 - media); // Calcula o naf e arredonda para cima
             } else {
-                situacao = "Aprovado!"; 
+                situation = "Aprovado!"; 
             }
-
-            return [situacao, media]
-
-        })
+        
+            console.log("Situação:", situation);
+            console.log("NAF:", naf);
+        
+            return [situation, naf];
+        });
 
         console.log(data.values);
         console.log(newData);
@@ -74,8 +95,6 @@ async function getSpreadsheetData() {
     }
 
 };
-
-
 
 getSpreadsheetData();
 
